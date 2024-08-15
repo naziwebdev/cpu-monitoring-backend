b46c5c0a9b50a75ac62a79be2795c39642350ace
nazi <nazipaeez777@gmail.com>
2024-08-15 22:35:14 +0330

    Config cors options

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const http = require("http");
 const os = require("os-utils");
+const cors = require('cors')
 const socketIo = require("socket.io");
 const { updateUpuUsage } = require("./utils/cpu");
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, PUT, POST, DELETE",
+  credentials: true,
+  allowedHeaders:
+    "Content-Type, Authorization, Content-Length, X-Requested-With",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5173/",
   },
 });
 
